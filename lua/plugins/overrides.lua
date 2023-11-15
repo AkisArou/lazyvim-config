a6d06c2592779640841d8e8337d1caad1adb19ff
Akis ARou <akisarou90@gmail.com>
2023-11-15 05:56:30 +0200

    asd

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -212,14 +212,7 @@ return {
       vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
       vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
       vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-      vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })(
-{
-        event = "file_added",
-        handler = function(arg)
-          -- do something, the value of arg varies by event.
-        end,
-        id = "optional unique id, only meaningful if you want to unsubscribe later",
-      })
+      vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
       require("neo-tree").setup({
         window = {
